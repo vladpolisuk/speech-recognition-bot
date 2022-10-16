35701a54f8f3bb5ea8e6bcf88bbe6eb0063ba59c
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-10-16 22:25:55 +0300

    fix code style

--- src/api.mjs
+++ src/api.mjs
@@ -1,4 +1,3 @@
-import axios from 'axios';
 export const TELEGRAM_API_KEY = "5428829984:AAFse4sGWam_AIrWiwNi3d-e4MDWIr21qwE";
 export const YANDEX_API_KEY = "AQVNznGPhwP2CJBnNYbneCjtQe-_aXVKqKq8Mweb";
 
